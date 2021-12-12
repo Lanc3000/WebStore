f7fa0584e63dcd9f5300be65cb909f2f7fd7d254
NPCBA\SamigulinTI <temurt_t@mail.ru>
2021-12-12 22:47:04 +0300

    Добавление HomeController

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,10 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddControllersWithViews();
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
 //Загрузка информации из файла конфигурации
 //var configuration = app.Configuration;
 //var props = configuration["CustomParam"];
+//app.MapGet("/", () => app.Configuration["CustomParam"]);
+app.MapGet("/throw", () =>
+{
+    throw new ApplicationException("Ошибка в программе.");
+});
 
-app.MapGet("/", () => app.Configuration["CustomParam"]);
-
+app.MapDefaultControllerRoute();
 app.Run();
