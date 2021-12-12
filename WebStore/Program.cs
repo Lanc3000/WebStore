9d71b7f359673b370feb4fd625275cfc932d99fa
NPCBA\SamigulinTI <temurt_t@mail.ru>
2021-12-12 22:27:30 +0300

    Динамическое изменение приветсвия через appsettings.json

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -2,10 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //Загрузка информации из файла конфигурации
-var configuration = app.Configuration;
+//var configuration = app.Configuration;
+//var props = configuration["CustomParam"];
 
-var props = configuration["CustomParam"];
-
-app.MapGet("/", () => props);
+app.MapGet("/", () => app.Configuration["CustomParam"]);
 
 app.Run();
