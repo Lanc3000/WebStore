535403da7c38aeaf16b16612489bd3f9a3735006
NPCBA\SamigulinTI <temurt_t@mail.ru>
2021-12-12 23:46:52 +0300

    Страница с сотрудниками

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _employees = new() 
+        {
+            new Employee { Id = 1, FirstName = "Иванов", LastName = "Иван", Patronymic = "Иванович", Age = 27},
+            new Employee { Id = 2, FirstName = "Петров", LastName = "Петр", Patronymic = "Александрович", Age = 24},
+            new Employee { Id = 3, FirstName = "Сидоров", LastName = "Владимир", Patronymic = "Юрьевич", Age = 36}
+        };
+
         public IActionResult Index()
         {
             //return Content("Hello from controller!");
@@ -13,6 +21,11 @@ namespace WebStore.Controllers
         public string ConfiguredAction(string id)
         {
             return $"akdgja - {id}";
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employees);
         }
     }
 }
