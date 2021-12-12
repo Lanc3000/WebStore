d88c66ca0991437f43b46fcf13b620af3785d5f2
NPCBA\SamigulinTI <temurt_t@mail.ru>
2021-12-12 18:38:00 +0300

    Добавление возможности чтения содержимого файла appsettings.json

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Загрузка информации из файла конфигурации
+var configuration = app.Configuration;
+
+var props = configuration["CustomParam"];
+
+app.MapGet("/", () => props);
 
 app.Run();
