7aa1e88dfc8ed535a9556b44d0555406d8f45928
NPCBA\SamigulinTI <temurt_t@mail.ru>
2021-12-14 00:22:27 +0300

    Класс реализующии интерфейс хранилища

---
+++ WebStore/Models/EmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace WebStore.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public IQueryable<Employee> Employees => new List<Employee>
+        {
+            new Employee { Id = 1, FirstName = "Иванов", LastName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, FirstName = "Петров", LastName = "Петр", Patronymic = "Александрович", Age = 24 },
+            new Employee { Id = 3, FirstName = "Сидоров", LastName = "Владимир", Patronymic = "Юрьевич", Age = 36 }
+        }.AsQueryable<Employee>();
+    }
+}
