9fbbf1dbdc80f92a7be8ddabaa65ce94e82521a3
NPCBA\SamigulinTI <temurt_t@mail.ru>
2021-12-15 00:06:03 +0300

    Home.Index разделен на частичные представления

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> __Employees;
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
