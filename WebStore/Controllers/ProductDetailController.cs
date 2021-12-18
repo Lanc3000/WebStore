beb088ea97c813b8b35af8dbea5e163f198991ce
NPCBA\SamigulinTI <temurt_t@mail.ru>
2021-12-18 23:21:01 +0300

    Добавлены разные контролеры и представления к ним

---
+++ WebStore/Controllers/ProductDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebStore/Views/ProductDetail/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebStore.Views.ProductDetail
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
