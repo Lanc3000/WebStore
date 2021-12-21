53f01ee28c83469740910f1f3662572fa660b3a7
Lanc3000 <temurt_t@mail.ru>
2021-12-21 22:44:16 +0300

    Пример работы промежуточного ПО

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,17 +1,19 @@
 using WebStore.Infrastructure.Conventions;
+using WebStore.Infrastructure.Middleware;
 using WebStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
-services.AddControllersWithViews(opt =>
-{
-    opt.Conventions.Add(new TestConvention());   
-});
+//services.AddControllersWithViews(opt =>
+//{
+//    opt.Conventions.Add(new TestConvention());   
+//});
 //services.AddMvc();
 //services.AddControllers(); WebApi
 //services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -20,20 +22,26 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
+//app.Map("/testpath", async context => await context.Response.WriteAsync("Test middleware")); Добавление промежуточного ПО простой способ
 //app.UseStaticFiles(new StaticFileOptions { ServeUnknownFileTypes = true}); 
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseMiddleware<TestMiddleware>(); // добаление промежуточного ПО более сложный способ
+
+app.UseWelcomePage("/welcome");
 //Загрузка информации из файла конфигурации
 //var configuration = app.Configuration;
 //var props = configuration["CustomParam"];
 //app.MapGet("/", () => app.Configuration["CustomParam"]);
+
 app.MapGet("/throw", () =>
 {
     throw new ApplicationException("Ошибка в программе.");
 });
 
-//app.MapDefaultControllerRoute();
+//app.MapDefaultControllerRoute(); - определение стандартного маршрута (внизу сейчас написано тоже самое)
 app.MapControllerRoute(name: "default",
                        pattern: "{controller=Home}/{action=Index}/{id?}");
 
