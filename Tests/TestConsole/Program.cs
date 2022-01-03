1477f35afcb8927e0fc5dfabaf51b4fc4383a276
Lanc3000 <temurt_t@mail.ru>
2022-01-03 19:24:32 +0300

    Примеры работы с коллекцией сервисов

---
+++ Tests/TestConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TestConsole.Data;
+using TestConsole.Services;
+using TestConsole.Services.Interfaces;
+
+var service_collections = new ServiceCollection();
+
+service_collections.AddScoped<IDataManager, DataManager>();
+service_collections.AddScoped<IDataProcessor, ConsolePrintProcessor>();
+
+//service_collections.AddSingleton<IDataManager, DataManager>();
+//service_collections.AddSingleton<IDataProcessor, WriteToFileProcessor>();
+//service_collections.AddSingleton<IDataProcessor, ConsolePrintProcessor>();
+
+//service_collection.AddTransient<>()
+//service_collection.AddScoped<>()
+
+var provider = service_collections.BuildServiceProvider();
+
+var service = provider.GetRequiredService<IDataManager>();
+
+using (var scope = provider.CreateScope())
+{
+    var scope_provider = scope.ServiceProvider;
+    var service2 = scope_provider.GetRequiredService<IDataManager>();
+    var is_equals = ReferenceEquals(service, service2);
+}
+    //var service2 = provider.GetRequiredService<IDataManager>();
+
+    //var is_Equals = ReferenceEquals(service, service2);
+
+    var data = Enumerable.Range(1, 100).Select(i => new DataValue
+    {
+        Id = i,
+        Value = $"Value - {i}",
+        Time = DateTime.Now.AddHours(-i * 10),
+    });
+
+service.ProcessData(data);
+
+Console.ReadLine();
---
+++ Tests/TestConsole/Services/DataManager.cs
@@ -0,0 +1,28 @@
+﻿using TestConsole.Data;
+using TestConsole.Services.Interfaces;
+
+namespace TestConsole.Services
+{
+    public class DataManager : IDataManager, IDisposable
+    {
+        private readonly IDataProcessor _Processor;
+
+        public DataManager(IDataProcessor processor)
+        {
+            _Processor = processor;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Менеджер обработки данных уничтожен");
+        }
+
+        public void ProcessData(IEnumerable<DataValue> Values)
+        {
+            foreach (var value in Values)
+            {
+                _Processor.Process(value);
+            }
+        }
+    }
+}
