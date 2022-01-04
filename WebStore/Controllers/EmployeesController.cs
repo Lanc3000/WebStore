bab99a16eb36dd6537d232fb2e45b85c71849cb5
Lanc3000 <temurt_t@mail.ru>
2022-01-04 11:56:08 +0300

    Валидация данных

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -65,6 +65,12 @@ namespace WebStore.Controllers
         [HttpPost]
         public IActionResult Edit(EmployeeViewModel Model) 
         {
+            if (Model.LastName == "Асама" && Model.Name == "Бен" && Model.Patronymic == "Ладен")
+                ModelState.AddModelError("", "Террористов на работу не берём!");
+
+            if (!ModelState.IsValid)
+                return View(Model);
+
             var employee = new Employee
             {
                 Id = Model.Id,
