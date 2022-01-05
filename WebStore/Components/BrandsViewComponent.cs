b276a9f4ac671b41a602971c12550ab14654c672
Lanc3000 <temurt_t@mail.ru>
2022-01-05 12:20:27 +0300

    Компонент брендов

--- WebStore/Components/BrandsViewComponent.cs
+++ WebStore/Components/BrandsViewComponent.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components;
 
 public class BrandsViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IProductData _ProductData;
+
+    public BrandsViewComponent(IProductData ProductData)
     {
-        return View();
+        _ProductData = ProductData;
     }
+
+    public IViewComponentResult Invoke() => View(GetBrands());
+
+    private IEnumerable<BrandViewModel> GetBrands() =>
+        _ProductData.GetBrands()
+            .OrderBy(x => x.Order)
+            .Select(x => new BrandViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+            });
 }
