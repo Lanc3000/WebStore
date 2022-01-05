23eef20633f65f802f9d32ffd9fbf9da37def124
Lanc3000 <temurt_t@mail.ru>
2022-01-05 13:31:04 +0300

    Добавление товаров в сервис каталога товаров

---
+++ WebStore/Services/InMemoryProductData.cs
@@ -0,0 +1,28 @@
+﻿using WebStore.Data;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Interfaces;
+
+public class InMemoryProductData : IProductData
+{
+    public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+    public IEnumerable<Section> GetSections() => TestData.Sections;
+
+    public IEnumerable<Product> GetProducts(ProductFilter? Filters = null)
+    {
+        IEnumerable<Product> query = TestData.Products;
+
+        //if (Filters?.SectionId != null)
+        //    query = query.Where(x => x.SectionId == Filters.SectionId);
+
+        if (Filters is { SectionId: var section_id }) // сахар из C#9
+            query = query.Where(x => x.SectionId == section_id);
+
+        if (Filters is { BrandId: var brand_id })
+            query = query.Where(x => x.BrandId == brand_id);
+
+        return query;
+    }
+}
