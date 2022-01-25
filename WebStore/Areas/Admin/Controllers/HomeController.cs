ad5edf9b0b2c2caa35f7f1f537246d3f19961f2e
Lanc3000 <temurt_t@mail.ru>
2022-01-25 23:48:54 +0300

    Подготовка инфраструктуры области администратора

---
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin")] //нужно указывать область, чтоб приложение понимало какой контролер вызывать
+    public class HomeController : Controller
+    {
+        public IActionResult Index => View();
+    }
+}
