2703ab972c6f72063be34f843e0d5374846c6325
Lanc3000 <temurt_t@mail.ru>
2022-01-26 22:20:37 +0300

    Подготовка к созданию редактора каталога товаров

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 
 namespace WebStore.Areas.Admin.Controllers
 {
-    [Area("Admin")] //нужно указывать область, чтоб приложение понимало какой контролер вызывать
+    [Area("Admin"), Authorize(Roles = Role.Administrators)] //нужно указывать область, чтоб приложение понимало какой контролер вызывать
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
